cebd01cb568bce5ae71c770d58037384b66782d9
Santhosh <santhom@thoughtworks.com>
2011-06-14 17:33:07 +0530

    Santhosh/Deepak | Added functional tests to verify server Configuration

---
+++ tests/functional/client.py
@@ -0,0 +1,58 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# Copyright 2010 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import httplib
+import socket
+import urllib
+
+
+class Client(object):
+
+    """A base client class - derived from Glance.BaseClient"""
+
+    def __init__(self, host='localhost', port='9292', use_ssl=False):
+        self.host = host
+        self.port = port
+        self.use_ssl = use_ssl
+
+    def get(self, path, params={}, headers={}):
+        return self._do_request("GET", path, params=params, headers=headers)
+
+    def _get_connection(self):
+        if self.use_ssl:
+            return httplib.HTTPSConnection(self.host, self.port)
+        else:
+            return httplib.HTTPConnection(self.host, self.port)
+
+    def _do_request(self, method, path, body=None, headers={}, params={}):
+
+        url = path + '?' + urllib.urlencode(params)
+
+        try:
+            connection = self._get_connection()
+            connection.request(method, url, body, headers)
+            response = connection.getresponse()
+            if(response.status < 400):
+                return response
+            else:
+                raise Exception("Server returned error: %s" % response.read())
+
+        except (socket.error, IOError), e:
+            raise Exception("Unable to connect to "
+                            "server. Got error: %s" % e)
---
+++ tests/functional/test_service.py
@@ -0,0 +1,44 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# Copyright 2010 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import unittest
+from tests.functional.client import Client
+
+
+class TestServiceConf(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_root_url_returns_versions(self):
+        response = self.client.get("/")
+
+        self.assertEqual(response.status, 200)
+        self.assertTrue("versions" in response.read())
+
+    def test_extensions_are_loaded(self):
+        response = self.client.get("/v0.1/extensions")
+
+        self.assertEqual(response.status, 200)
+        self.assertTrue("extensions" in response.read())
+
+    def test_ipam_service_can_be_accessed(self):
+        response = self.client.get("/v0.1/ipam/ip_blocks")
+
+        self.assertEqual(response.status, 200)
+        self.assertTrue("ip_blocks" in response.read())

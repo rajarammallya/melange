3f009a9a56a8467c2b03e4c8e6c2cd43e62b4681
Santhosh <santhom@thoughtworks.com>
2011-06-15 17:43:52 +0530

    Santhosh/Deepak | Moved the service restart to functional tests setup

--- tests/functional/__init__.py
+++ tests/functional/__init__.py
@@ -14,3 +14,10 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
+from tests.functional.server import Server
+
+
+def setup():
+    print "Restarting melange server..."
+    Server('melange').restart()

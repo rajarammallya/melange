c10cde1cb84ac9a3c8f8ba71711471047fa2fe78
Santhosh <santhom@thoughtworks.com>
2011-07-01 18:19:02 +0530

    Santhosh/Deepak | Added the CLI tests for private and tenant based ip_blocks

--- tests/functional/test_ip_block_cli.py
+++ tests/functional/test_ip_block_cli.py
@@ -23,7 +23,7 @@ from melange.ipam.models import IpBlock
 
 from tests.functional import execute
 from tests.functional import get_api_port
-from tests.factories.models import PublicIpBlockFactory
+from tests.factories.models import PublicIpBlockFactory, PrivateIpBlockFactory
 from tests import BaseTest
 
 
@@ -59,6 +59,71 @@ class TestPublicIpBlockCLI(BaseTest):
         ip_block = PublicIpBlockFactory()
 
         exitcode, out, err = run("public_ip_block delete %s" % ip_block.id)
+
+        self.assertEqual(exitcode, 0)
+        self.assertTrue(IpBlock.get(ip_block.id) is None)
+
+
+class TestPrivateIpBlockCLI(BaseTest):
+
+    def test_create(self):
+        exitcode, out, err = run("private_ip_block create 10.1.1.0/29")
+
+        self.assertEqual(exitcode, 0)
+        ip_block = IpBlock.get_by(cidr="10.1.1.0/29", type='private')
+        self.assertTrue(ip_block is not None)
+
+    def test_list(self):
+        exitcode, out, err = run("private_ip_block list")
+
+        self.assertEqual(exitcode, 0)
+        self.assertIn("ip_blocks", out)
+
+    def test_show(self):
+        ip_block = PrivateIpBlockFactory(tenant_id=None)
+
+        exitcode, out, err = run("private_ip_block show %s" % ip_block.id)
+
+        self.assertEqual(exitcode, 0)
+        self.assertIn(ip_block.cidr, out)
+
+    def test_delete(self):
+        ip_block = PrivateIpBlockFactory(tenant_id=None)
+
+        exitcode, out, err = run("private_ip_block delete %s" % ip_block.id)
+
+        self.assertEqual(exitcode, 0)
+        self.assertTrue(IpBlock.get(ip_block.id) is None)
+
+
+class TestTenantIpBlockCLI(BaseTest):
+
+    def test_create(self):
+        exitcode, out, err = run("tenant_ip_block create 123 10.1.1.0/29")
+
+        self.assertEqual(exitcode, 0)
+        ip_block = IpBlock.get_by(cidr="10.1.1.0/29",
+                                  type="private", tenant_id="123")
+        self.assertTrue(ip_block is not None)
+
+    def test_list(self):
+        exitcode, out, err = run("tenant_ip_block list 123")
+
+        self.assertEqual(exitcode, 0)
+        self.assertIn("ip_blocks", out)
+
+    def test_show(self):
+        ip_block = PrivateIpBlockFactory(tenant_id=123)
+
+        exitcode, out, err = run("tenant_ip_block show 123 %s" % ip_block.id)
+
+        self.assertEqual(exitcode, 0)
+        self.assertIn(ip_block.cidr, out)
+
+    def test_delete(self):
+        ip_block = PrivateIpBlockFactory(tenant_id=123)
+
+        exitcode, out, err = run("tenant_ip_block delete 123 %s" % ip_block.id)
 
         self.assertEqual(exitcode, 0)
         self.assertTrue(IpBlock.get(ip_block.id) is None)

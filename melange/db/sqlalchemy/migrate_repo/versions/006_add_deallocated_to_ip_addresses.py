c7c62f2f45cc6388272c22d3809cae2bd44faeb2
Rajaram Mallya <rajarammallya@gmail.com>
2011-08-23 11:03:41 +0530

    Rajaram/Vinkesh | Fixed some pep8 errors

--- melange/db/sqlalchemy/migrate_repo/versions/006_add_deallocated_to_ip_addresses.py
+++ melange/db/sqlalchemy/migrate_repo/versions/006_add_deallocated_to_ip_addresses.py
@@ -39,4 +39,5 @@ def upgrade(migrate_engine):
 def downgrade(migrate_engine):
     meta = MetaData()
     meta.bind = migrate_engine
-    Table('ip_addresses', meta, autoload=True).columns["marked_for_deallocation"].drop()
+    Table('ip_addresses', meta,
+          autoload=True).columns["marked_for_deallocation"].drop()

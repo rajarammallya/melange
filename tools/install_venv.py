a5c5c8d0e7528cc04d804a926f8f133063a99dbf
Jason Kölker <jason@koelker.net>
2011-11-01 13:54:47 -0500

    remove the manual install of openstack-skeleton since openstack.common is now in pip-requires

--- tools/install_venv.py
+++ tools/install_venv.py
@@ -105,10 +105,6 @@ def install_dependencies(venv=VENV):
               'greenlet'], redirect_output=False)
     run_command(['tools/with_venv.sh', 'pip', 'install', '-E', venv, '-r',
               PIP_REQUIRES], redirect_output=False)
-    run_command(['tools/with_venv.sh', 'pip', 'install', '-E', venv, '-e',
-                 "git+https://github.com/rajarammallya/"
-                 "openstack-skeleton.git#egg=skeleton"],
-                redirect_output=False)
 
     # Tell the virtual env how to "import melange"
     pthfile = os.path.join(venv, "lib", PY_VERSION, "site-packages",
